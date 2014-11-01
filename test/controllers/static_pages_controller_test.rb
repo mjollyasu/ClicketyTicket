31643d33004b42e2008b7f94010bb674ed8b569b
mjollyasu <mjolly@asu.edu>
2014-11-01 02:39:55 +0000

    Finish static page content

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get landing" do
     get :landing
     assert_response :success
+    assert_select "title", "Landing | Clickety Ticket"
   end
 
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Clickety Ticket"
   end
 
 end
