18c1e79dac2d76f3b7952790f03263a7d69c3ecd
mjollyasu <mjolly@asu.edu>
2014-11-01 03:58:40 +0000

    Complete landing layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #returns the full title for a page if present, supplies default otherwise
+  def full_title(page_title = '')
+    base_title = "Clickety Ticket"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
