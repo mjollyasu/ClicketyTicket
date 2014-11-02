2c8ce96073d666600910bf7bf0bdd85441e06adb
mjollyasu <mjolly@asu.edu>
2014-11-02 12:10:33 +0000

    Bug test fix for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root   'static_pages#landing'
+  get 'home' => 'static_pages#home'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
