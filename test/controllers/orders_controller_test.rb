f02c12b657b30c9128a6037049614c51aea57418
Shaun M. Brown <shaunmbrown@gmail.com>
2014-11-07 08:51:26 +0000

    Clear excess order code

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,34 +5,34 @@ class OrdersControllerTest < ActionController::TestCase
     @order = orders(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:orders)
-  end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:orders)
+  # end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test "should create order" do
-    assert_difference('Order.count') do
-      post :create, order: { event_id: @order.event_id, tickets_purchased: @order.tickets_purchased, user_id: @order.user_id }
-    end
+  # test "should create order" do
+  #   assert_difference('Order.count') do
+  #     post :create, order: { event_id: @order.event_id, tickets_purchased: @order.tickets_purchased, user_id: @order.user_id }
+  #   end
 
-    assert_redirected_to order_path(assigns(:order))
-  end
+  #   assert_redirected_to order_path(assigns(:order))
+  # end
 
-  test "should show order" do
-    get :show, id: @order
-    assert_response :success
-  end
+  # test "should show order" do
+  #   get :show, id: @order
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get :edit, id: @order
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit, id: @order
+  #   assert_response :success
+  # end
 
   test "should update order" do
     patch :update, id: @order, order: { event_id: @order.event_id, tickets_purchased: @order.tickets_purchased, user_id: @order.user_id }
