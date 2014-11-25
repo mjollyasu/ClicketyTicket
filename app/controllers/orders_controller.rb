eb2d08d24a4de1b8e5f4e5181d2d31b4da374fe4
Shaun M. Brown <shaunmbrown@gmail.com>
2014-11-25 21:25:37 +0000

    Orders troubleshooting 5

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,8 +27,11 @@ class OrdersController < ApplicationController
   def create
     
     @order = Order.new(order_params)
-    #@order.user_id = $current_user.id
-    @order.user_id = session[:user_id]
+    
+    $current_user ||= User.find_by(id: session[:user_id])
+    @order.user_id = $current_user.id
+    
+    #@order.user_id = session[:user_id]
     @order.event_id = $current_event.id
     
     respond_to do |format|
